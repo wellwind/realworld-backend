8b738b7aed77908317b06cd3708a7a3553f8b812
黃升煌 Mike <mike@miniasp.com>
2019-04-26 16:37:19 +0800

    放寬 cors

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ router.post(
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
